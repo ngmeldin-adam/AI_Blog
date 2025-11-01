0e7bc9f481c80707c99e77866815280f634ffde9
ngmeldin-adam <101349423+ngmeldin-adam@users.noreply.github.com>
2025-11-01 14:24:42 +0300

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,7 +7,8 @@ const Navbar = () =>{
         <div className="flex justify-between items-center py-5 mx-8 sm:mx-20
         xl:mx-32">
             {/* <img onClick={()=>navigate("/")} src={assets.logo} alt="logo" className="w-32 sm:w-44 cursor-pointer" /> */}
-            <h1 className="w-32 sm:w-44 cursor-pointer sm:text-[px] text-[35px] font-bold   text-primary" onClick={()=> navigate("/")}>AI BLog </h1>
+            <h1 className="w-32 sm:w-44 cursor-pointer sm:text-[px] text-[35px] font-bold 
+                text-primary bg-gradient-to-r from-[#217bfe] to-[#8B5CF6] bg-clip-text text-transparent" onClick={()=> navigate("/")}>AI BLog </h1>
             <button onClick={()=>navigate("/admin ")}  className="flex items-center gap-2 rounded-full text-sm
             cursor-pointer bg-primary text-white px-7 py-2.5">{token ? "Dashboard" : "Login"}
                 <img src={assets.arrow} alt="" />
